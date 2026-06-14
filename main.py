c0e513bd4da357f5eeaeb6446a6df1fa273681c6
Alexandru <lexyus22@gmail.com>
2026-06-14 14:07:20 +0200

    Add account search functionality

--- main.py
+++ main.py
@@ -59,6 +59,25 @@ def add():
             password_entry.delete(0, END)
 
 
+def search_account():
+    website = website_entry.get()
+    try:
+        with open('my_accounts.json') as my_accounts:
+            accounts = json.load(my_accounts)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error', message='No Data File Found.')
+
+    else:
+        if website in accounts:
+            email = accounts[website]['email']
+            password = accounts[website]['password']
+            messagebox.showinfo(title= website, message=f'Email: {email}\n'
+                                                            f'Password: {password}')
+        else:
+            messagebox.showinfo(title='Error', message=f'No details for {website} exists.')
+
+
 window = Tk()
 window.title('Password Manager')
 window.config(padx= 100, pady= 50, bg= '#C0E1D2')
@@ -70,12 +89,16 @@ logo_img = logo_img.subsample(4,4)
 canvas.create_image(200, 200, image= logo_img)
 canvas.grid(row=0, column=0, columnspan=3)
 
+
+
 website_label = Label(text='Website:', bg='#C0E1D2', font= FONT)
 website_label.grid(row=1, column=0, sticky="w")
 
 website_entry = Entry(width= 50)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2, sticky="ew", ipady=5)
+website_entry.grid(row=1, column=1, sticky="ew", ipady=5)
+
+
 
 email_label = Label(text='Email / Username:', bg= '#C0E1D2', font= FONT)
 email_label.grid(row=2, column=0, sticky="w")
@@ -84,18 +107,27 @@ email_entry = Entry(width= 50)
 email_entry.insert(0, 'example_email@example.com')
 email_entry.grid(row=2, column=1, columnspan=2, sticky="ew", ipady=5)
 
+
+
 password_label = Label(text= 'Password:', bg= '#C0E1D2', font= FONT)
 password_label.grid(row=3, column=0, sticky="w")
 
 password_entry = Entry(width= 25)
 password_entry.grid(row=3, column=1, sticky="ew", ipady=5)
 
-generate_password_button = Button(text= 'Generate Password', command=generate_password, bg= '#DBE4C9')
-generate_password_button.config(padx=1, pady=0)
+
+
+search_button = Button(text= 'Search', command=search_account, width= 20, bg= '#DBE4C9')
+search_button.config(padx=2, pady=2)
+search_button.grid(row=1, column=2, padx=(6, 0))
+
+generate_password_button = Button(text= 'Generate Password', width= 20, command=generate_password, bg= '#DBE4C9')
+generate_password_button.config(padx=2, pady=2)
 generate_password_button.grid(row=3, column=2, padx=(6, 0))
 
 add_button = Button(text= 'Add', command= add, width= 42, bg= '#DBE4C9')
 add_button.config(padx= 2, pady= 2)
 add_button.grid(row=4, column=1, columnspan=2, sticky="ew")
+
 
 window.mainloop()
